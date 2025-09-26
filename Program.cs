06f2c32ee398d0e1006a4007f3c08341309e03db
pyr0xd <kalkestatimoty@gmail.com>
2025-09-26 15:56:44 +0200

    I added a switch
    i started with a switch to start somewhere

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+Console.WriteLine("hello");
+string choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        Console.WriteLine("login");
+        break;
+    case "2":
+        Console.WriteLine("add User");
+        break;
+    case "3":
+        Console.WriteLine("add item");
+        break;
+    case "4":
+        Console.WriteLine("trade item");
+        break;
+    case "5":
+        Console.WriteLine("hello");
+        break;
+    default:
+        break;
+}
